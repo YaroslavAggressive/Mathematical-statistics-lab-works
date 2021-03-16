b389c33521b64ef55091cf6a4872894028e0bb45
YaroslavAggressive <55752143+YaroslavAggressive@users.noreply.github.com>
2021-03-16 13:01:43 +0500

    Update all functions of creating all dependencies

--- Lab1(creating samples)/main.py
+++ Lab1(creating samples)/main.py
@@ -103,13 +103,3 @@ def uniform_distribution():
     plt.show()
     return
 
-
-def first_main_task():
-    normal_distribution()  # normal distribution for all sizes
-    cauchy_distribution()  # cauchy distribution for all sizes
-    laplace_distribution()  # laplace distribution for all sizes
-    poisson_distribution()  # poisson distribution for all sizes
-    uniform_distribution()  # uniform distribution for all sizes
-
-
-first_main_task()  # all generated samples from task (for 5 distributions)
